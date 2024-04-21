74b0f46c66da09d84ac68c972968eed3584b0665
Evgeniy <none>
2024-04-21 21:02:09 +0300

    Add TextChangedListener fot editTextSearch

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,10 +1,13 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.example.playlistmaker.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
@@ -12,6 +15,16 @@ class SearchActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivitySearchBinding.inflate(layoutInflater)
     }
+
+    private val editTextSearch by lazy {
+        binding.editTextSearch
+    }
+
+    private val clearButton by lazy {
+        binding.imageViewClearButton
+    }
+
+    private var searchInput = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +37,17 @@ class SearchActivity : AppCompatActivity() {
         }
 
         binding.toolbar.setNavigationOnClickListener { finish() }
+
+        editTextSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.isVisible = !s.isNullOrEmpty()
+                searchInput = editTextSearch.text.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
 
     }
 }
