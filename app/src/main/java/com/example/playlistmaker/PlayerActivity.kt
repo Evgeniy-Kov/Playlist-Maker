1e7df2523fbb565db9cfc28517015326ecc4ef3b
Evgeniy <none>
2024-06-23 00:12:10 +0300

    Add factory method to PlayerActivity.kt

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -1,17 +1,21 @@
 package com.example.playlistmaker
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.playlistmaker.databinding.ActivityPlayerBinding
+import com.google.gson.Gson
 
 class PlayerActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityPlayerBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,6 +24,16 @@ class PlayerActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    companion object {
+        private const val EXTRA_TRACK_ITEM = "extra_track_item"
+        fun newIntent(context: Context, track: Track): Intent {
+            val intent = Intent(context, PlayerActivity::class.java).apply {
+                putExtra(EXTRA_TRACK_ITEM, Gson().toJson(track))
+            }
+            return intent
         }
     }
 }
