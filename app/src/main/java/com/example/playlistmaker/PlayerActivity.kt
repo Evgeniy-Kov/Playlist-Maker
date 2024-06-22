933b8dcf28e34a0b8ae645b8bbcd5aa4276120f4
Evgeniy <none>
2024-06-23 00:21:16 +0300

    Initialize views in PlayerActivity

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -7,6 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.databinding.ActivityPlayerBinding
 import com.google.gson.Gson
 
@@ -27,7 +30,12 @@ class PlayerActivity : AppCompatActivity() {
             insets
         }
 
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
         parseIntent()
+        initializeViews()
     }
 
     private fun parseIntent() {
@@ -37,7 +45,33 @@ class PlayerActivity : AppCompatActivity() {
         track = Gson().fromJson(intent.getStringExtra(EXTRA_TRACK_ITEM), Track::class.java)
     }
 
+    private fun initializeViews() {
+        Glide.with(this)
+            .load(track.getHighQualityCoverLink())
+            .placeholder(R.drawable.placeholder)
+            .centerCrop()
+            .transform(
+                RoundedCorners(convertDpToPx(resources.displayMetrics, COVER_CORNER_RADIUS_IN_DP))
+            )
+            .into(binding.ivCover)
+
+        binding.apply {
+            tvTrackName.text = track.trackName
+            tvArtistName.text = track.artistName
+            tvTrackTime.text = track.getFormattedTime()
+            if (track.collectionName.isNullOrBlank()) {
+                groupCollectionName.isVisible = false
+            } else {
+                tvCollectionName.text = track.collectionName
+            }
+            tvYear.text = track.getFormattedYear()
+            tvGenre.text = track.primaryGenreName
+            tvCountry.text = track.country
+        }
+    }
+
     companion object {
+        private const val COVER_CORNER_RADIUS_IN_DP = 8f
         private const val EXTRA_TRACK_ITEM = "extra_track_item"
         fun newIntent(context: Context, track: Track): Intent {
             val intent = Intent(context, PlayerActivity::class.java).apply {
