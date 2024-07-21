bfb8c7b82833823fb264552205a4221726c10849
Evgeniy <none>
2024-07-21 20:24:23 +0300

    Add DarkThemeModeRepositoryImpl class

---
+++ app/src/main/java/com/example/playlistmaker/data/reposirtory/DarkThemeModeRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.example.playlistmaker.data.reposirtory
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import com.example.playlistmaker.domain.api.DarkThemeModeRepository
+import com.example.playlistmaker.domain.model.DarkThemeMode
+
+class DarkThemeModeRepositoryImpl(private val preferences: SharedPreferences) :
+    DarkThemeModeRepository {
+
+    override fun getDarkThemeMode(): DarkThemeMode {
+        val darkThemeModeString = preferences.getString(
+            DARK_THEME_MODE_KEY, null
+        ) ?: DarkThemeMode.FOLLOW_SYSTEM.name
+        return DarkThemeMode.valueOf(darkThemeModeString)
+    }
+
+    override fun saveDarkThemeMode(darkThemeMode: DarkThemeMode) {
+        preferences.edit {
+            putString(
+                DARK_THEME_MODE_KEY, darkThemeMode.name
+            )
+        }
+    }
+
+    private companion object {
+        private const val DARK_THEME_MODE_KEY = "dark_theme_mode_key"
+    }
+}
