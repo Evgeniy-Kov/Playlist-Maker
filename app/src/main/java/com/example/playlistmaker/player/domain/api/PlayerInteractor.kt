1eac378ebcdc1a9a3eb6ef142be323d7b5ce78da
Evgeniy <none>
2024-08-05 22:45:13 +0300

    Add PlayerInteractor interface

---
+++ app/src/main/java/com/example/playlistmaker/player/domain/api/PlayerInteractor.kt
@@ -0,0 +1,16 @@
+package com.example.playlistmaker.player.domain.api
+
+import com.example.playlistmaker.player.domain.model.PlayerState
+
+interface PlayerInteractor {
+
+    fun play()
+
+    fun pause()
+
+    fun prepare(dataSource: String, observer: StatusObserver)
+
+    fun release()
+
+    fun getPlayerState(): PlayerState
+}
