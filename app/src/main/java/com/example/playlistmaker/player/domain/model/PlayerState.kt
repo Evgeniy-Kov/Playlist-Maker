ce8bb0dd06f98b1f96e2ca658334155038901066
Evgeniy <none>
2024-08-05 22:35:29 +0300

    Add PlayerState enum

---
+++ app/src/main/java/com/example/playlistmaker/player/domain/model/PlayerState.kt
@@ -0,0 +1,8 @@
+package com.example.playlistmaker.player.domain.model
+
+enum class PlayerState {
+    PLAYER_STATE_DEFAULT,
+    PLAYER_STATE_PREPARED,
+    PLAYER_STATE_PLAYING,
+    PLAYER_STATE_PAUSED
+}
