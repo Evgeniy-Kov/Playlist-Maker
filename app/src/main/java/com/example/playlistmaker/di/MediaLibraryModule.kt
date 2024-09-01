685b6e1a5d4f3a063e72443585f5012031c297a0
Evgeniy <none>
2024-09-01 13:03:55 +0300

    Add PlaylistsFragment and viewModel for it

--- app/src/main/java/com/example/playlistmaker/di/MediaLibraryModule.kt
+++ app/src/main/java/com/example/playlistmaker/di/MediaLibraryModule.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker.di
 
 import com.example.playlistmaker.library.ui.FavouriteTracksViewModel
+import com.example.playlistmaker.library.ui.PlaylistsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -8,5 +9,9 @@ val mediaLibraryModule = module {
 
     viewModel {
         FavouriteTracksViewModel()
+    }
+
+    viewModel {
+        PlaylistsViewModel()
     }
 }
