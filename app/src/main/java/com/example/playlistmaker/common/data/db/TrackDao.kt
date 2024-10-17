1fb04068b90d6297ba37213823758cf6bd060fba
Evgeniy <none>
2024-10-17 16:16:59 +0300

    Add TrackDao interface

---
+++ app/src/main/java/com/example/playlistmaker/common/data/db/TrackDao.kt
@@ -0,0 +1,20 @@
+package com.example.playlistmaker.common.data.db
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface TrackDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTrack(track: TrackEntity)
+
+    @Query("SELECT * FROM track_table")
+    suspend fun getTracks(): List<TrackEntity>
+
+    @Delete()
+    suspend fun deleteTrack(track: TrackEntity)
+}
