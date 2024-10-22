7866c2230320cff2abfe568ba55d361d4d2ffd08
Evgeniy <none>
2024-10-22 16:58:29 +0300

    Add dependencies for DB to Koin

--- app/src/main/java/com/example/playlistmaker/di/CommonModule.kt
+++ app/src/main/java/com/example/playlistmaker/di/CommonModule.kt
@@ -1,13 +1,31 @@
 package com.example.playlistmaker.di
 
 import androidx.room.Room
+import com.example.playlistmaker.common.data.converters.TrackDbConverter
 import com.example.playlistmaker.common.data.db.AppDatabase
+import com.example.playlistmaker.common.data.db.DbConfig
+import com.example.playlistmaker.common.data.repository.TracksRepositoryImpl
+import com.example.playlistmaker.common.domain.api.TracksInteractor
+import com.example.playlistmaker.common.domain.api.TracksRepository
+import com.example.playlistmaker.common.domain.impl.TracksInteractorImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val commonModule = module {
     single {
-        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db")
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, DbConfig.DATABASE_NAME)
             .build()
+    }
+
+    factory {
+        TrackDbConverter()
+    }
+
+    single<TracksRepository> {
+        TracksRepositoryImpl(get(), get())
+    }
+
+    factory<TracksInteractor> {
+        TracksInteractorImpl(get())
     }
 }
