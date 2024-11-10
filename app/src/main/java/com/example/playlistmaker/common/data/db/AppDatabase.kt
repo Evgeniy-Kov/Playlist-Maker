497730f559656214326c8fd215dfe6d46033488a
Evgeniy <none>
2024-11-10 21:05:38 +0300

    Update AppDatabase class

--- app/src/main/java/com/example/playlistmaker/common/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/playlistmaker/common/data/db/AppDatabase.kt
@@ -2,10 +2,24 @@ package com.example.playlistmaker.common.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.playlistmaker.common.data.db.playlist.PlaylistDao
+import com.example.playlistmaker.common.data.db.playlist.PlaylistEntity
+import com.example.playlistmaker.common.data.db.playlist.PlaylistTrackCrossRef
+import com.example.playlistmaker.common.data.db.playlist.PlaylistTrackEntity
 
-@Database(version = 1, entities = [TrackEntity::class])
+@Database(
+    version = 1,
+    entities = [
+        TrackEntity::class,
+        PlaylistEntity::class,
+        PlaylistTrackEntity::class,
+        PlaylistTrackCrossRef::class
+    ]
+)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun favouriteTrackDao(): TrackDao
+
+    abstract fun playlistDao(): PlaylistDao
 
 }
