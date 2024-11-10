17ac7fd4f92eddc2688aa34b3a9b48a8168f674b
Evgeniy <none>
2024-11-10 23:53:42 +0300

    Add addOnDestinationChangedListener to HostActivity

--- app/src/main/java/com/example/playlistmaker/main/ui/HostActivity.kt
+++ app/src/main/java/com/example/playlistmaker/main/ui/HostActivity.kt
@@ -1,10 +1,11 @@
 package com.example.playlistmaker.main.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
@@ -14,18 +15,36 @@ class HostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         val binding = ActivityHostBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            when (destination.id) {
+                R.id.newPlaylistFragment -> {
+                    binding.bottomNavigationView.isVisible = false
+                    window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+                }
+
+                R.id.playerFragment -> {
+                    binding.bottomNavigationView.isVisible = false
+                    window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+                }
+
+                else -> {
+                    binding.bottomNavigationView.isVisible = true
+                    window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
+                }
+            }
+        }
         binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
