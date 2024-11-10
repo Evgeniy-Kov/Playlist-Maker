7dca4d791b6ef2cafd9f3d042e16d3aa2a8a699c
Evgeniy <none>
2024-11-10 21:07:24 +0300

    Add PlaylistInteractor interface

---
+++ app/src/main/java/com/example/playlistmaker/common/domain/api/PlaylistInteractor.kt
@@ -0,0 +1,19 @@
+package com.example.playlistmaker.common.domain.api
+
+import com.example.playlistmaker.common.domain.model.Playlist
+import com.example.playlistmaker.common.domain.model.Track
+import kotlinx.coroutines.flow.Flow
+
+interface PlaylistInteractor {
+
+    suspend fun addPlaylist(playlist: Playlist)
+
+    suspend fun addTrackToPlaylist(track: Track, playlist: Playlist)
+
+    fun getPlaylists(): Flow<List<Playlist>>
+
+    fun getPlaylistWithTracks(): Flow<List<Track>>
+
+    fun getTrackWithPlaylists(trackId: Long): Flow<List<Playlist>>
+
+}
