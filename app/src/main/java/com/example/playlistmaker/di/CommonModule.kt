57eb34a2301992f6dc68739366e7d7c6912ccb8f
Evgeniy <none>
2024-11-10 21:10:28 +0300

    Add new dependencies to CommonModule

--- app/src/main/java/com/example/playlistmaker/di/CommonModule.kt
+++ app/src/main/java/com/example/playlistmaker/di/CommonModule.kt
@@ -1,12 +1,18 @@
 package com.example.playlistmaker.di
 
 import androidx.room.Room
+import com.example.playlistmaker.common.data.converters.PlaylistDbConverter
+import com.example.playlistmaker.common.data.converters.PlaylistTrackDbConverter
 import com.example.playlistmaker.common.data.converters.TrackDbConverter
 import com.example.playlistmaker.common.data.db.AppDatabase
 import com.example.playlistmaker.common.data.db.DbConfig
+import com.example.playlistmaker.common.data.repository.PlaylistRepositoryImpl
 import com.example.playlistmaker.common.data.repository.TracksRepositoryImpl
+import com.example.playlistmaker.common.domain.api.PlaylistInteractor
+import com.example.playlistmaker.common.domain.api.PlaylistRepository
 import com.example.playlistmaker.common.domain.api.TracksInteractor
 import com.example.playlistmaker.common.domain.api.TracksRepository
+import com.example.playlistmaker.common.domain.impl.PlaylistInteractorImpl
 import com.example.playlistmaker.common.domain.impl.TracksInteractorImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -27,5 +33,21 @@ val commonModule = module {
 
     factory<TracksInteractor> {
         TracksInteractorImpl(get())
+    }
+
+    factory {
+        PlaylistDbConverter()
+    }
+
+    factory {
+        PlaylistTrackDbConverter()
+    }
+
+    single<PlaylistRepository> {
+        PlaylistRepositoryImpl(get(), get(), get())
+    }
+
+    factory<PlaylistInteractor> {
+        PlaylistInteractorImpl(get())
     }
 }
