9fc44766065a158c0862cd984f22f7734847cb3b
Evgeniy <none>
2024-11-10 20:57:56 +0300

    Add PlaylistTrackEntity class

--- app/src/main/java/com/example/playlistmaker/common/data/db/DbConfig.kt
+++ app/src/main/java/com/example/playlistmaker/common/data/db/DbConfig.kt
@@ -4,4 +4,5 @@ object DbConfig {
     const val DATABASE_NAME = "database.db"
     const val FAVOURITE_TRACKS_TABLE_NAME = "favourite_tracks_table"
     const val PLAYLISTS_TABLE_NAME = "playlists_table"
+    const val PLAYLIST_TRACK_TABLE_NAME = "playlist_track_table"
 }
